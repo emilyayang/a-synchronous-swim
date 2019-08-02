7968744e6a7d5eb352d1c627874a14612a011682
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-08-02 16:15:14 -0700

    working on connecting to server

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -7,20 +7,6 @@ const multipart = require('./multipartUtils');
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 ////////////////////////////////////////////////////////
 
-const ajaxGetSwim = (file) => {
-  $.ajax({
-    type: 'GET',
-    data: formData,
-    url: 'http://127.0.0.1:8080/?',
-    cache: false,
-    contentType: false,
-    processData: false,
-    success: () => {
-      // reload the page
-      window.location = window.location.href;
-    }
-  });
-};
 let messageQueue = null;
 module.exports.initialize = (queue) => {
   messageQueue = queue;
@@ -28,7 +14,13 @@ module.exports.initialize = (queue) => {
 
 module.exports.router = (req, res, next = () => { }) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  res.writeHead(200, headers);
-  res.end();
-  next(); // invoke next() at the end of a request to help with testing!
-};
+  if (req.method === 'GET') {
+    let allMoves = ['left', 'right', 'up', 'down'];
+    let randomMove = allMoves[Math.floor(Math.random() * 4)];
+    res.end(randomMove);
+  }
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200, headers);
+    res.end();
+  }
+}
